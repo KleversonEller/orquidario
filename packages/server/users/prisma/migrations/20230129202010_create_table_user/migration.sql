a5a8c3b104c87f79a43b98b492ca3dae33e63ef7
Tamiris Shigaki <shigakiendo@gmail.com>
2023-01-29 17:27:31 -0300

    feat: :building_construction: definindo esquema do banco de dados, criando migrations e conectando com o DB

---
+++ packages/server/users/prisma/migrations/20230129202010_create_table_user/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" BIGINT NOT NULL,
+    "permission" TEXT NOT NULL DEFAULT 'user',
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
