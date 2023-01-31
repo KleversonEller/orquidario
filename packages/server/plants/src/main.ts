446aa0c461be8de0e658931197e0c5711bffec7d
Kleverson Eller <kleverson.a16@gmail.com>
2023-01-30 22:20:16 -0300

    chore: :zap: Importando e implementando dependencias

--- packages/server/plants/src/main.ts
+++ packages/server/plants/src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './models/app.module';
+
+const port = process.env.PORT || 3013;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(port, () => console.log(`Rodando na porta ${port}`));
 }
 bootstrap();
